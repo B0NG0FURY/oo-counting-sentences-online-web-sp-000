b5940634f0f101a12541a4f1ee0db412e62d2334
John Troutman <jtroutmanman@gmail.com>
2020-09-27 15:57:26 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    count = []
+    if self.sentence? || self.question? || self.exclamation?
+      test = self.split
+      
+    else
+      return 0
+    end
   end
 end
